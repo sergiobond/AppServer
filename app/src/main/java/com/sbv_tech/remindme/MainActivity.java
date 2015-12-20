9dc75844909b57df480adb6942e5f4f1a60bc953
Sergey Bondarenok <bondarjonok@yahoo.de>
2015-12-21 01:07:53 +0200

    added MainActivity

---
+++ app/src/main/java/com/sbv_tech/remindme/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sbv_tech.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sergey.bondarenok on 12/21/15.
+ */
+public class MainActivity extends Activity {
+
+ @Override
+    protected void onCreate(Bundle savedInstanceState){
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.activity_main);
+ }
+}
